355aa9c713a9086c03a44009d6c4004c35fd1667
Alphawizz Flutter Team <alphawizzflutterteam@gmail.com>
2024-08-22 17:15:59 +0530

    first commit

---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,21 @@
+final String appName = "Super Mart";
+
+final String packageName = "com.supermart.user";
+final String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+final String iosPackage = "com.supermart.user";
+final String iosLink = 'your ios link here';
+final String appStoreId = "com.supermart.user";
+
+final String deepLinkUrlPrefix = 'https://alpha.ecommerce.link';
+final String deepLinkName = 'alpha.ecommerce.link';
+
+final int timeOut = 50;
+const int perPage = 10;
+
+// final String baseUrl = 'https://buransmart.in/app/v1/api/';
+final String baseUrl = 'https://developmentalphawizz.com/super_mart/app/v1/api/';
+
+final String imageUrl = 'https://developmentalphawizz.com/super_mart/';
+
+final String jwtKey = "e1e421dceee9ea718ab764ea4fdef95629e7f415";
